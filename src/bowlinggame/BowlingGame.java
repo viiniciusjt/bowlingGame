9940cbe05c0ffda90705e07409613fbc0dad72db
Vinicius <Vinicius@Vinicius-PC>
2014-11-20 16:33:44 -0200

    Parte 2

--- src/bowlinggame/BowlingGame.java
+++ src/bowlinggame/BowlingGame.java
@@ -1,23 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package bowlinggame;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Vinicius
  */
 public class BowlingGame {
+    Integer aux = 0;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        // new 
+        ModeloBowling b = new ModeloBowling();
+       Integer pontuacao = 0; 
+        Scanner in = new Scanner(System.in);
+        
+        for (int rodada = 1; rodada <= 10; rodada++) {
+            pontuacao = 10;
+            for (int jogada =1 ; jogada <= 2; jogada++) {
+                System.out.println("Informe a Pontuação da Jogada: " + jogada + " da rodada " + rodada);
+                b.pontuacao = in.nextInt();
+            }
+        }
+        
+       
     }
+    
+     public void jogadaExtra(){
+            
+        }
+     public Integer calcularPontos(Integer pontuacao) {
+         return aux;
+     }
+     
+     
     
 }
---
+++ src/bowlinggame/ModeloBowling.java
@@ -0,0 +1,39 @@
+package bowlinggame;
+
+/**
+ *
+ * @author Vinicius
+ */
+public class ModeloBowling {
+
+    public Integer pontuacao;
+    public String consultarPOntos;
+    public Integer pontosGeral;
+
+    public Integer getPontuacao() {
+        return pontuacao;
+    }
+
+    public void setPontuacao(Integer pontuacao) {
+        this.pontuacao = pontuacao;
+    }
+
+    public String getConsultarPOntos() {
+        return consultarPOntos;
+    }
+
+    public void setConsultarPOntos(String consultarPOntos) {
+        this.consultarPOntos = consultarPOntos;
+    }
+
+    public Integer getPontosGeral() {
+        return pontosGeral;
+    }
+
+    public void setPontosGeral(Integer pontosGeral) {
+        this.pontosGeral = pontosGeral;
+    }
+    
+    
+
+}
